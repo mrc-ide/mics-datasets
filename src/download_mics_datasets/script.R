1dc9c258b2f774c26780cb65a92b5db278f31adc
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2020-04-04 16:45:54 +0100

    download dataset task

--- src/download_mics_datasets/script.R
+++ src/download_mics_datasets/script.R
@@ -1,92 +1,93 @@
 
-# provided dataset from https://mics.unicef.org/surveys
+## provided dataset from https://mics.unicef.org/surveys
 csv <- read.csv("surveys_catalogue.csv")
 
-# generate urls and file paths
+## indices of available datasets
+is_available <- csv$datasets == "Available"
+
+## generate candidate urls and file paths
 stem <- "https://mics-surveys-prod.s3.amazonaws.com"
 full <- mapply(FUN = file.path, stem, csv$round, csv$region, csv$country, csv$year, "Datasets",
                mapply(paste, csv$country, csv$round, "SPSS Datasets.zip"))
 urls <- vapply(full, FUN = URLencode, FUN.VALUE = character(1))
 csv$urls <- urls
-csv$path <- paste0("mics_surveys/", csv$country, "_", csv$round, "_", csv$year, ".rds")
+csv$urls[!is_available] <- NA
 
-# subset to available
-csv <- csv[which(csv$datasets == "Available"), ]
 
-# annoyingly the urls are not quite accurate so use downloaded htmls (sadly)
-fls <- list.files("htmls/", full.names = TRUE)
-fls <- grep("html", fls, value = TRUE)
-fls[1]
+## Download URLS do not follow a standardised format, so parse downloaded htmls
+## identify download links.
 
-links <- unlist(lapply(fls, function(x){ 
-  grep("Datasets",grep("amazon",rvest::html_attr(rvest::html_nodes(xml2::read_html(x),"a"),"href"),value=TRUE),value=TRUE)
-}))
+html_files <- list.files("htmls", full.names = TRUE)
+html_files <- html_files[file_ext(html_files) == "html"]
 
-# matching function
-match_clean <- function(a,b, quiet=TRUE){
-  a <- gsub("[[:punct:][:space:]]","",tolower(stringi::stri_trans_general(a, "latin-ascii")))
-  b <- gsub("[[:punct:][:space:]]","",tolower(stringi::stri_trans_general(b, "latin-ascii")))
-  ret <- match(a,b)
-  if(sum(is.na(ret)>0)){
-    dists <- stringdist::seq_distmatrix(lapply(a,utf8ToInt),lapply(b,utf8ToInt))
-    ret[is.na(ret)] <- apply(dists[which(is.na(ret)),,drop=FALSE],1,which.min)
+
+find_dataset_links <- function(x) {
+  a_nodes <- rvest::html_nodes(xml2::read_html(x), "a")
+  hrefs <- rvest::html_attr(a_nodes, "href")
+  dataset_links <- grep("amazonaws.*Datasets", hrefs, value=TRUE)
+}
+
+links <- unlist(lapply(html_files, find_dataset_links))
+
+if(length(links) != sum(is_available))
+  stop("Number of dataset links does not match number of available datasets.\n",
+       "These used to be aligned. Check your work")
+
+## matching function
+match_clean <- function(a, b, quiet=TRUE){
+  a <- gsub("[[:punct:][:space:]]", "", tolower(stringi::stri_trans_general(a, "latin-ascii")))
+  b <- gsub("[[:punct:][:space:]]", "", tolower(stringi::stri_trans_general(b, "latin-ascii")))
+  ret <- match(a, b)
+  if(sum(is.na(ret) > 0)){
+    dists <- stringdist::seq_distmatrix(lapply(a, utf8ToInt), lapply(b, utf8ToInt))
+    ret[is.na(ret)] <- apply(dists[which(is.na(ret)), , drop=FALSE], 1, which.min)
     if(!quiet){
-      print(unique(cbind(a,b[ret])))
+      print(unique(cbind(a, b[ret])))
     }
   }
   return(ret)
 }
+csv$urls[is_available] <- links[match_clean(dirname(csv$urls[is_available]), dirname(links))]
+csv$filename[is_available] <- vapply(basename(csv$urls[is_available]), URLdecode, character(1))
 
-csv$urls <- links[match_clean(dirname(csv$urls), dirname(links))]
+urls_tmp <- csv$urls
+csv$urls <- NULL
+csv$urls <- urls_tmp
 
-# update yml
+
+## Save survey catalogue CSV with filenames
+write.csv(csv, "mics_survey_catalogue_filenames.csv", row.names = FALSE, na = "")
+
+
+## Identify datasets to download
+save_dir <- "mics_datasets_raw"
+
+urls <- csv$urls[is_available]
+paths <- file.path(save_dir, csv$filename[is_available])
+
+## update orderly.yml with all identified artefacts
 yml <- yaml::read_yaml("orderly.yml")
-yml$artefacts[[1]]$data$filenames <- csv$path
+yml$artefacts[[2]]$data$filenames <- paths
 yaml::write_yaml(yml, "orderly.yml")
 
-# fucntion to download mics
-download_and_parse_survey <- function(url, path) {
-  
-  # get the zip
+download_survey <- function(url, path) {
+  print(basename(path))
+
+  ## get the zip
   tf <- tempfile()
   resp <- httr::GET(url,
                     destfile = tf,
-                    httr::write_disk(tf, overwrite = TRUE),
-                    httr::progress()
-  )
+                    httr::write_disk(tf, overwrite = TRUE)
+                    )
   
   code <- httr::status_code(resp)
-  if (code >= 400 && code < 600) {
-   return(-1) 
-  } else {
   
-  # extract contents and read in .savs
-  tf2 <- tempdir()
-  zip_contents <- unzip(tf, exdir = tf2)
-  files <- grep("sav", zip_contents, value = TRUE)
-  file_type <- gsub("^(.*)\\.sav$", "\\1", basename(files))
-  
-  # parse these into a named list
-  res <- lapply(files, function(x){
-    
-    res <- tryCatch(
-      haven::read_spss(x),
-      error=function(e) e
-    )
-    
-    if(inherits(res, "error")){
-      res <- foreign::read.spss(x)
-    }
-    return(res)
-  })
-    
-  names(res) <- file_type
-  
-  # save to file
-  saveRDS(res, path)
+  if ( !(code >= 400 && code < 600) ) {
+    file.copy(tf, path)
   }
+
+  return(code)
 }
 
-# download surveys
-dir.create("mics_surveys")
-surveys <- mapply(download_and_parse_survey, csv$urls, csv$path)
+dir.create(save_dir)
+surveys <- mapply(download_survey, urls, paths)
